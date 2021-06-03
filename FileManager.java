bee2d566ccea83e1719628c8f128b5131e064213
wangzhe04 <67325944+wangzhe04@users.noreply.github.com>
2021-06-03 17:39:36 +0800

    Add files via upload
    Making the program able to read the date format of M/DD/yyyy

--- FileManager.java
+++ FileManager.java
@@ -1,3 +1,8 @@
+///////////////////////File Header///////////////////////////
+//Author: Zhe Wang
+//Email: zwang2428@wisc.edu
+//Term: Summer2020
+////////////////////////////////////////////////////////////
 package application;
 
 import java.io.BufferedReader;
@@ -6,24 +11,47 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
+/**
+ * This class read in and add file into the dataStorage
+ * @author Zhe Wang
+ *
+ */
 public class FileManager {
 	public Stage primaryStage;
 	DataStorage a;
 	public FileManager(File file, DataStorage a) throws IOException, NumberFormatException {
-		//try {
-			//this.a = a;
-		//a = new DataStorage();
 		String line = "";
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		while((line = br.readLine()) != null){
 			String[] array = line.split(",");
+			System.out.println(array[0]);
+			
+			if(array[0].contains("/")) {
+				try {
+					//System.out.println(array[0]);
+					String start_dt = "2011-01-01";
+					DateFormat formatter = new SimpleDateFormat("M/DD/yyyy"); 
+					Date date = (Date)formatter.parse(array[0]);
+					SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-M-DD");
+					array[0] = newFormat.format(date);
+					//System.out.println(array[0]);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					continue;
+				}
+				
+			}
+			
 			if(array[2].equals("weight"))
 					continue;
 			if(a.contains(array[1])) {
@@ -33,53 +61,14 @@ public class FileManager {
 				newFarm.add(Integer.parseInt(array[2]), array[0]);
 				a.insert(array[1], newFarm);
 			}
-			//System.out.println(array[0]);
 		}
-		//}catch(NumberFormatException e1) {
-			
-		//}catch(Exception e) {
-			//e.printStackTrace();
-			//a = this.a;
-			
-			//System.out.println("Invalid File Input");
-		
 	}
-	
-	/*public void addFile( File file) {
-		try {
-		String line = "";
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		while((line = br.readLine()) != null){
-			String[] array = line.split(",");
-			if(array[2].equals("weight"))
-					continue;
-			if(a.contains(array[1])) {
-				a.get(array[1]).add(Integer.parseInt(array[2]), array[0]);
-			}else {
-				Farm newFarm = new Farm();
-				newFarm.add(Integer.parseInt(array[2]), array[0]);
-				a.insert(array[1], newFarm);
-			}
-			System.out.println(array[1]);
-		}
-		}catch(Exception e) {
-			a = new DataStorage();
-			System.out.println("Invalid File Input");
-		}
-	}*/ 
-	
-	/*public void invalidInput(Stage primaryStage) {
-		Label invalidIn = new Label("Invalid Input ");
-		Pane root = new Pane(invalidIn);
-		root.getChildren().add(invalidIn);
-		invalidIn.relocate(180, 180);
-		Scene mainScene = new Scene(root, 600, 400);
-		System.out.println("Invalid inputs");
-    	primaryStage.setScene(mainScene);
-    	primaryStage.show();
-	}*/
+
 	public DataStorage getStorage() {
 		return this.a;
 	}
+	
+	private static final SimpleDateFormat sdf = 
+            new SimpleDateFormat("yyyy-MM-dd");
 
 }
